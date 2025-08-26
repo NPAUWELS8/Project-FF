067cb0e13d1f744ab1b5050f84bcda57eda1b5f2
NPAUWELS8 <144807305+NPAUWELS8@users.noreply.github.com>
2025-08-27 01:15:17 +0200

    Update todos

--- src/constants/GamesConstant.jsx
+++ src/constants/GamesConstant.jsx
@@ -102,7 +102,6 @@ class Box {
     }
 }
 
-//TODO: fix longer control explanation to get smaller than other control explanations when screen is very small (e.g. for the sudoku game)
 const Controls = ({controlArray}) =>{
     const controls = controlArray.map((control,index)=>
         // <div key={index} className="flex flex-row">{icon}<p><b>{control.key}</b>{`: ${control.action}`}</p></div>
